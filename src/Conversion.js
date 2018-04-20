183a18a12abe5f923cc2b828cf316b650f9aff57
Bobby Martin <robert.martin.619@gmail.com>
2018-04-20 15:06:05 -0700

    Refactor Conversion.js constructor and prototypes

--- src/Conversion.js
+++ src/Conversion.js
@@ -2,25 +2,29 @@ import { YearsToSeconds } from './YearsToSeconds.js';
 class AgeConversion {
   constructor(age) {
     this.age = age;
+    this.mercury = .24;
+    this.venus = .62;
+    this.mars = 1.88;
+    this.jupiter = 11.86;
   }
   ageInSeconds() {
     const seconds = this.age * YearsToSeconds.prototype.yearSecondsMath();
     return seconds;
   }
   ageOnMercury() {
-    const mercuryAge = this.age / .24;
+    const mercuryAge = this.age / this.mercury;
     return mercuryAge;
   }
   ageOnVenus() {
-    const venusAge = this.age / .62;
+    const venusAge = this.age / this.venus;
     return venusAge;
   }
   ageOnMars() {
-    const marsAge = this.age / 1.88;
+    const marsAge = this.age / this.mars;
     return marsAge;
   }
   ageOnJupiter() {
-    const jupiterAge = this.age / 11.86;
+    const jupiterAge = this.age / this.jupiter;
     return jupiterAge;
   }
 }
