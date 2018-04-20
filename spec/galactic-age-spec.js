0ad7a38e7782a1b22dd38f3a9db339cfc5eabce2
Bobby Martin <robert.martin.619@gmail.com>
2018-04-20 12:17:05 -0700

    Add meaningful fail for fourth spec

--- spec/galactic-age-spec.js
+++ spec/galactic-age-spec.js
@@ -11,4 +11,7 @@ describe('Galactic Age', function() {
   it('should get current date in milliseconds', function() {
     expect(Date.parse("April 20, 2018") / 1000).toEqual(1524207600);
   });
+  it('should get current date in milliseconds', function() {
+    expect(Date.farse("August 28, 1987") / 1000).toEqual(557132400);
+  });
 });
