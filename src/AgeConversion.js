7c3dea503e1d01cea8779e8210da9773b5386362
Bobby Martin <robert.martin.619@gmail.com>
2018-04-20 16:14:13 -0700

    Rename AgeConversion keys to better represent what they are

--- src/AgeConversion.js
+++ src/AgeConversion.js
@@ -2,25 +2,25 @@ import { YearsToSeconds } from './YearsToSeconds.js';
 class AgeConversion {
   constructor(age) {
     this.age = age;
-    this.mercury = .24;
-    this.venus = .62;
-    this.mars = 1.88;
-    this.jupiter = 11.86;
+    this.mercuryYear = .24;
+    this.venusYear = .62;
+    this.marsYear = 1.88;
+    this.jupiterYear = 11.86;
   }
   ageInSeconds() {
     return this.age * YearsToSeconds.prototype.yearSecondsMath();
   }
   ageOnMercury() {
-    return this.age / this.mercury;
+    return this.age / this.mercuryYear;
   }
   ageOnVenus() {
-    return this.age / this.venus;
+    return this.age / this.venusYear;
   }
   ageOnMars() {
-    return this.age / this.mars;
+    return this.age / this.marsYear;
   }
   ageOnJupiter() {
-    return this.age / this.jupiter;
+    return this.age / this.jupiterYear;
   }
 }
 
