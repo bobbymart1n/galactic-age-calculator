e4c2dc2dd5ee809ce56641d094183ccedcd625be
Bobby Martin <robert.martin.619@gmail.com>
2018-04-20 12:10:43 -0700

    Add second spec test

--- spec/galactic-age-spec.js
+++ spec/galactic-age-spec.js
@@ -1,7 +1,11 @@
-const date = new Date();
-describe('Age', function() {
+describe('Galactic Age', function() {
+  let date;
 
   it('should get current date', function() {
+    date = new Date();
     expect(date.toDateString()).toEqual("Fri Apr 20 2018");
+  });
+  it('should get current date in milliseconds', function() {
+    expect(Date.parse("April 20, 2018")).toEqual(1524207600000);
   });
 });
