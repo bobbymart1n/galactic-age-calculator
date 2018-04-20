5e2183566ea76b74fc1df054c23ed22b8727d182
Bobby Martin <robert.martin.619@gmail.com>
2018-04-20 14:22:03 -0700

    Delete content from main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
-import { Conversion } from './Conversion.js';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(function() {
-  const dateTest = new Conversion(30);
-  console.log(dateTest.ageInSeconds());
 });
